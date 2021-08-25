2425989242f2d8cb8c8c1025a2685ddbdb38f662
TommyIrmia <87779182+TommyIrmia@users.noreply.github.com>
2021-08-25 15:55:37 +0300

    Update app-header.jsx

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -7,7 +7,7 @@ export function AppHeader() {
             <h1>TriApp</h1>
         </Link>
         <div className="logos">
-            <Link to={'/'} className="book-logo"><img src="../../img/booklogo.png" /></Link>
+            <Link to={'/'} className="book-logo"><img src="../img/booklogo.png" /></Link>
             <Link to={'/mail'} className="mail-logo"><img src="../../img/maillogo.png" /></Link>
             <Link to={'/keep'} className="keep-logo"><img src="../../img/keeplogo.png" /></Link>
         </div>
