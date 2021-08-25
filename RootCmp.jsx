f8d9eef3c2af27e39385243f6592fb11880f2cfe
תומי ירמיה <tommyirmia@gmail.com>
2021-08-25 11:55:22 +0300

    added header

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,16 +1,21 @@
 const Router = ReactRouterDOM.HashRouter;
 const { Route, Switch } = ReactRouterDOM;
 
-
+import { Home } from './js/Home.jsx'
+import { AppHeader } from './js/cmps/app-header.jsx'
 
 export function App() {
     return (
         <Router>
             <header>
+                <AppHeader />
             </header>
 
             <section className="main-layout">
                 <Switch>
+                    {/* <Route path="/keep" component={KeepApp} /> */}
+                    {/* <Route path="/mail" component={MailApp} /> */}
+                    {/* <Route path="/book" component={BookApp} /> */}
                     <Route path="/" component={Home} />
                 </Switch>
             </section>
---
+++ js/Home.jsx
@@ -0,0 +1,10 @@
+
+
+export function Home () {
+
+    return (
+        <main>
+            <h1>TriApp</h1>
+        </main>
+    )
+}
