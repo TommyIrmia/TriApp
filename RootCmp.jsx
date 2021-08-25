3ae1760bae91be4f5d7f7eb29b1dbeea8425e81b
tomer morad <tomer.morad771@gmail.com>
2021-08-25 14:09:16 +0300

    add note app file

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -5,9 +5,6 @@ import { Home } from './js/Home.jsx'
 import { AppHeader } from './js/cmps/app-header.jsx'
 import { AppFooter } from './js/cmps/app-footer.jsx'
 import { NoteApp } from './apps/note-app.jsx'
-import { Home } from './pages/Home.jsx'
-import { AppHeader } from './cmps/app-header.jsx'
-import { AppFooter } from './cmps/app-footer.jsx'
 import { MailApp } from './apps/tri-mail/pages/mail-app.jsx'
 
 export function App() {
@@ -19,7 +16,7 @@ export function App() {
 
             <section className="main-layout">
                 <Switch>
-                    <Route path="/keep" component={NoteApp} />
+                    {/* <Route path="/keep" component={NoteApp} /> */}
                     {/* <Route path="/mail" component={MailApp} /> */}
                     {/* <Route path="/book" component={BookApp} /> */}
                     <Route path="/" component={Home} />
