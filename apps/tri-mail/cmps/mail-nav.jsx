11702fb456dfb6dbcf751a457312ca3ef4cebcb3
תומי ירמיה <tommyirmia@gmail.com>
2021-08-25 15:59:29 +0300

    fixed source of imgs

--- apps/tri-mail/cmps/mail-nav.jsx
+++ apps/tri-mail/cmps/mail-nav.jsx
@@ -5,11 +5,11 @@ export class MailNav extends React.Component {
     render() {
         return (
             <section className="mail-nav">
-                <div>nav</div>
-                <div>nav</div>
-                <div>nav</div>
-                <div>nav</div>
-                <div>nav</div>
+                <div>Compose</div>
+                <div>Inbox</div>
+                <div>Starred</div>
+                <div>Sent</div>
+                <div>Drafts</div>
             </section>
         )
     }
--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -1,15 +1,15 @@
-const {Link} = ReactRouterDOM;
+const { Link } = ReactRouterDOM;
 
 export function AppHeader() {
     return (<section className="app-header">
         <Link to={'/'} className="main-logo clean">
-            <img src="../../img/logo.png" />
+            <img src="./img/logo.png" />
             <h1>TriApp</h1>
         </Link>
         <div className="logos">
-            <Link to={'/'} className="book-logo"><img src="../../img/booklogo.png" /></Link>
-            <Link to={'/mail'} className="mail-logo"><img src="../../img/maillogo.png" /></Link>
-            <Link to={'/keep'} className="keep-logo"><img src="../../img/keeplogo.png" /></Link>
+            <Link to={'/'} className="book-logo"><img src="./img/booklogo.png" /></Link>
+            <Link to={'/mail'} className="mail-logo"><img src="./img/maillogo.png" /></Link>
+            <Link to={'/keep'} className="keep-logo"><img src="./img/keeplogo.png" /></Link>
         </div>
     </section>
     )
