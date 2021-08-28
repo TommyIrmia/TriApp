2da9ac87ece9b90c33ddf3da079d7d1bf6455ea7
tomer morad <tomer.morad771@gmail.com>
2021-08-29 01:35:56 +0300

    add nav

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -6,7 +6,7 @@ export function Home() {
         <main className="home">
 
             <Link to={'/mail'} className="app-preview app1 clean">
-                <div className="app-img"> here will be an img of the site</div>
+                <div className="app-img"> <img src="./img/mail-app1.png"/> </div>
                 <div className="app-info">
                     <h1>TriMail</h1>
                     <h3>An app to manage your emails the best way possible! Tri it, you won't regeret it ;)!</h3>
@@ -14,7 +14,7 @@ export function Home() {
             </Link>
 
             <Link to={'/keep'} className="app-preview app2 clean">
-                <div className="app-img"> here will be an img of the site</div>
+                <div className="app-img"> <img src="./img/keep-app.png" /> </div>
                 <div className="app-info">
                     <h1>TriKeep</h1>
                     <h3>An app that will let you keep all of your important thoughts, desires and passions all in one place! </h3>
