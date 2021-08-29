3565e746c9f121cc0575afa47556894b0e86bee2
תומי ירמיה <tommyirmia@gmail.com>
2021-08-29 03:49:17 +0300

    added cool stuff and final commit

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,21 +3,28 @@ const { Link } = ReactRouterDOM;
 export class Home extends React.Component {
 
     state = {
-        idx: 0,
+        bookIdx: 0,
+        mailIdx: 0,
+        keepIdx: 0,
     }
 
-    mailAppImgs = ["./img/mail-app.png", "./img/keep-app.png", "./img/book-app.png", "./img/keep-app.png"]
-    keepAppImgs = []
-    bookAppImgs = []
+    mailAppImgs = ["./img/mail-app.png", "./img/mail-app1.png", "./img/mail-app2.png", "./img/mail-app3.png"]
+    keepAppImgs = ["./img/keep-app.png", "./img/keep-app1.png", "./img/keep-app2.png", "./img/keep-app3.png"]
+    bookAppImgs = ["./img/book-app.png", "./img/book-app1.png", "./img/book-app2.png", "./img/book-app3.png"]
     inter;
 
-    setChangeImage = () => {
-        let { idx } = this.state
+    setChangeImage = (type) => {
+        let idx;
+        if (type === 'mail') idx = this.state.mailIdx;
+        if (type === 'book') idx = this.state.bookIdx;
+        if (type === 'keep') idx = this.state.keepIdx;
         this.inter = setInterval(() => {
             idx++
             if (idx > 3) idx = 0;
-            console.log(idx);
-            this.setState({ idx })
+            console.log(idx, type);
+            if (type === 'mail') this.setState({ mailIdx: idx });
+            if (type === 'book') this.setState({ bookIdx: idx });
+            if (type === 'keep') this.setState({ keepIdx: idx });
         }, 1000)
     }
 
@@ -28,24 +35,24 @@ export class Home extends React.Component {
         return (
             <main className="home" >
 
-                <Link to={'/mail'} className="app-preview app1 clean">
-                    <div className="app-img"> <img src="./img/mail-app.png" /> </div>
+                <Link to={'/mail'} className="app-preview app1 clean" onMouseEnter={() => this.setChangeImage('mail')} onMouseLeave={this.stopChangeImage}>
+                    <div className="app-img"> <img src={this.mailAppImgs[this.state.mailIdx]} /> </div>
                     <div className="app-info">
                         <h1>TriMail</h1>
                         <h3>An app to manage your emails the best way possible! Tri it, you won't regeret it ;)!</h3>
                     </div>
                 </Link>
 
-                <Link to={'/keep'} className="app-preview app2 clean" onMouseEnter={this.setChangeImage} onMouseLeave={this.stopChangeImage}>
-                    <div className="app-img"> <img src={this.mailAppImgs[this.state.idx]} /> </div>
+                <Link to={'/keep'} className="app-preview app2 clean" onMouseEnter={() => this.setChangeImage('keep')} onMouseLeave={this.stopChangeImage}>
+                    <div className="app-img"> <img src={this.keepAppImgs[this.state.keepIdx]} /> </div>
                     <div className="app-info">
                         <h1>TriKeep</h1>
                         <h3>An app that will let you keep all of your important thoughts, desires and passions all in one place! </h3>
                     </div>
                 </Link>
 
-                <Link to={'/book'} className="app-preview app3 clean">
-                    <div className="app-img"><img src="./img/book-app.png" /></div>
+                <Link to={'/book'} className="app-preview app3 clean" onMouseEnter={() => this.setChangeImage('book')} onMouseLeave={this.stopChangeImage}>
+                    <div className="app-img"><img src={this.bookAppImgs[this.state.bookIdx]} /></div>
                     <div className="app-info">
                         <h1>TriBook</h1>
                         <h3>An app that will contain all of your favorite books! add new books, filter thorugh them and ofcouse add reviews!</h3>
