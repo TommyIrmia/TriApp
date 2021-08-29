cc8d8688f2db2c6a3f461ac84509c1f2e84aa2e0
tomer morad <tomer.morad771@gmail.com>
2021-08-29 03:29:22 +0300

    fix footer

--- cmps/app-footer.jsx
+++ cmps/app-footer.jsx
@@ -1,11 +1,11 @@
-
+const {  Link } = ReactRouterDOM;
 
 export function AppFooter() {
 
     return (
         <section className="app-footer">
             <div className="main-logo">
-                <img src="././img/logo.png" />
+               <img src="././img/logo.png" /> 
                 <h1>TriApp</h1>
             </div>
                 <p><small>© All rights reserved to Tomer Morad & Tommy Irmia ©</small></p>
